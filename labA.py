978be8c888f0f65b24a3e854dda7a16fdb8dbb9b
unknown <josemarianoreyesh02@gmail.com>
2023-02-20 18:06:28 -0600

    Update labA.py

--- labA.py
+++ labA.py
@@ -1,8 +1,6 @@
 """
-Generador de Automatas AFN y AFD a partir de una REGEX
+Generador de Automatas AFN de una REGEX
 José Mariano Reyes 20074
-Andrea Lam 20102
-Kenneth Gálvez 20079
 """
 
 from Regex_Postfix import convertExpression
@@ -10,7 +8,6 @@ from Postfix_AFN import PostifixToAFN
 
 # main del programa
 if __name__ == '__main__':
-    # Expresion de prueba: (b|b)*abb(a|b)*
     # Simbolo epsilon  ε
     exp = input("\nIngrese una expresion a convertir: ")
 
